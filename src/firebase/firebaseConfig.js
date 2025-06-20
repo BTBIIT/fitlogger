679db87ac5404e498eb8a356a8ca1aa301c0bcc8
BTBIIT <rjsgur789@gmail.com>
2025-06-20 17:08:43 +0900

    feat: Firebase 설정을 .env로 분리하고 보안 강화

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
+// Firebase SDK import
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// .env 파일에 저장된 환경변수로 구성
 const firebaseConfig = {
-  apiKey: "AIzaSyD3M3uPL-za8rf5P064O3ZmqCTH4XKYczE",
-  authDomain: "fitlogger-ac483.firebaseapp.com",
-  projectId: "fitlogger-ac483",
-  storageBucket: "fitlogger-ac483.firebasestorage.app",
-  messagingSenderId: "820987746811",
-  appId: "1:820987746811:web:a93af9bbace984b1fb135e",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
+// Firebase 앱 초기화
 const app = initializeApp(firebaseConfig);
-
 export default app;
